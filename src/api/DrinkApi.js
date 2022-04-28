f82f6c57e42646404a0997b2424f4b22c0431075
Alan Puglisi <alan.c.puglisi@gmail.com>
2022-04-28 10:56:34 -0500

    fix drinkapi

--- src/api/DrinkApi.js
+++ src/api/DrinkApi.js
@@ -1,35 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 class DrinkApi {
 	static API_BASE = `https://www.thecocktaildb.com/api/json/v1/1`;
 
-	static async request(endpoint, data = {}, method = 'get') {
-		const url = `${this.API_BASE}/${endpoint}`;
-		console.debug('API Call:', url, data, method);
-		const params = method === 'get' ? data : {};
-
+	static async getRandomDrink() {
 		try {
-			return (await axios({ url, method, data, params })).data;
-		} catch (err) {
-			console.error('API Error:', err.response);
-			let message = err.response.data.error.message;
-			throw Array.isArray(message) ? message : [ message ];
+			const res = await axios.get(`${this.API_BASE}/random.php`);
+			return res.data["drinks"][0];
+		} catch (e) {
+			console.log(e);
+			throw Error(e);
 		}
 	}
 
-	static async getRandomDrink() {
-		const res = await this.request('random.php');
-		return res.data['drinks'][0];
-	}
-
 	static async searchByName(name) {
-		const res = await this.request('search.php', { s: name });
-		return res['drinks'];
+		try {
+			const res = await axios.get(`${this.API_BASE}/search.php?s=${name}`);
+			return res["drinks"];
+		} catch (e) {
+			console.log(e);
+			throw Error(e);
+		}
 	}
 
 	static async getDrinkById(id) {
-		const res = await this.request('lookup.php', { i: id });
-		return res['drinks'][0];
+		try {
+			const res = await this.request(`${this.API_BASE}/lookup.php?i=${id}`);
+			return res["drinks"][0];
+		} catch (e) {
+			console.log(e);
+			throw Error(e);
+		}
 	}
 }
 export default DrinkApi;
